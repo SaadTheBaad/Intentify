cdb9ab3e1f98896b9143ed4817e7981b586384ed
Saad Sheikh <sheikh.saad.aqeel@gmail.com>
2026-01-17 00:56:49 -0700

    adding intents tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,11 @@
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
-  return <Tabs />;
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="history" options={{ title: "History" }} />
+      <Tabs.Screen name="intents" options={{ title: "Intents" }} />
+    </Tabs>
+  );
 }
